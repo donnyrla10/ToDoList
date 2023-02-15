87f134e49449de4151d26888e8cee0fcb494a64e
donnyrla10 <kokoo1214@naver.com>
2023-02-15 21:58:42 +0900

    [chore] preview provider 추가

--- ToDoListSnapKit/ToDoListSnapKit/ToDoListViewController.swift
+++ ToDoListSnapKit/ToDoListSnapKit/ToDoListViewController.swift
@@ -6,10 +6,31 @@
 //
 
 import UIKit
-import SnapKit
+import SnapKit //auto layout
+import SwiftUI //preview provider
 
 class ToDoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+//preview provider를 통해 UI 화면 바로 확인
+struct ToDoListViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            let toDoListViewController = ToDoListViewController()
+            return UINavigationController(rootViewController: toDoListViewController)
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
+        
+        typealias UIViewControllerType = UIViewController
     }
 }
