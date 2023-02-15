e9f9fa276da21e592579bd3e725e05cbbbb0ca96
donnyrla10 <kokoo1214@naver.com>
2023-02-15 22:20:43 +0900

    [feat] 네비게이션바 커스텀

--- ToDoListSnapKit/ToDoListSnapKit/ToDoListViewController.swift
+++ ToDoListSnapKit/ToDoListSnapKit/ToDoListViewController.swift
@@ -12,6 +12,19 @@ import SwiftUI //preview provider
 class ToDoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureNavigationBar() //네비게이션바 커스텀
+    }
+    
+    func configureNavigationBar() {
+        //navigation bar
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true //scrolling이 일어나면 네비게이션 바를 가리도록
+        //navigation bar button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: nil, action: nil)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: nil, action: nil)
     }
 }
 
@@ -27,9 +40,7 @@ struct ToDoListViewController_Previews: PreviewProvider {
             return UINavigationController(rootViewController: toDoListViewController)
         }
         
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-            
-        }
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
         
         typealias UIViewControllerType = UIViewController
     }
