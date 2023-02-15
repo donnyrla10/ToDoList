02aa5d2055ae267d573336cf0c73dadf1c211365
donnyrla10 <kokoo1214@naver.com>
2023-02-15 21:52:04 +0900

    project setting

---
+++ ToDoListSnapKit/ToDoListSnapKit/ToDoListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListViewController.swift
+//  ToDoListSnapKit
+//
+//  Created by 김영선 on 2023/02/15.
+//
+
+import UIKit
+import SnapKit
+
+class ToDoListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
